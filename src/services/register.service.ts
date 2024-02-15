6ff383776189f95de5f8309902d66464cb88e27e
Yago Ferreira <yago.fgomes@gmail.com>
2024-02-15 12:41:34 +0000

    finalizando a rota de user com tratativa de erros e desacoplamento com controllers, repositories e services

---
+++ src/services/register.service.ts
@@ -0,0 +1,31 @@
+import { UsersRepository } from '@/repositories/users-repository';
+import { hash } from 'bcryptjs';
+import { UserAlreadyExistsError } from './erros/user-already-exists-error';
+
+interface registerService {
+    name: string,
+    email: string,
+    password: string
+}
+
+export class RegisterUseCase{
+
+    constructor(private usersRepository: UsersRepository){}
+
+    async execute({name, email, password}:registerService){
+        const password_hash = await hash(password, 6);
+    
+        const userWithSameEmail = await this.usersRepository.findByEmail(email);
+
+        if(userWithSameEmail){
+            throw new UserAlreadyExistsError();
+        }
+
+        await this.usersRepository.create({
+            name,
+            email,
+            password_hash
+        });
+    }
+}
+
